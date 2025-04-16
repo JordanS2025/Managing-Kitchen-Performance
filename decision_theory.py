670d01caf4dbf4f18fe38d3d3e392257064f44bb
BenC2026 <BCroyle1@my.gcu.edu>
2025-04-16 11:53:32 -0700

    Update decision_theory.py

--- decision_theory.py
+++ decision_theory.py
@@ -2,4 +2,15 @@
 # Topic: Decision Theory
 
 graph = [[0.25, 0.35, 0.4], [0.35, 0.15, 0.5], [0.4, 0.5, 0.1]]
-if graph
+if graph[0][0] >= 5:
+    print("Discounts are available")
+if graph[0][1] >= 5:
+    print("Prices remain the same")
+if graph[0][2] >= 5:
+    print("Prices are higher than usual")
+if graph[1][0] >= 5:
+    print("We can work with less workers than usual")
+if graph[1][1] >= 5:
+    print("Usual number of workers required")
+if graph[1][2] >= 5:
+    print("We need more workers than usual")
